b99425340e2fca0dd426318f6ad55ff59be2a428
zyt4321 <zyt4321@qq.com>
2017-09-11 21:51:23 +0800

    mongodb Auth

--- config/mymongoose.js
+++ config/mymongoose.js
@@ -2,8 +2,9 @@ var mongoose = require("mongoose");
 var config = require("./config");
 
 module.exports = function () {
-    var db = mongoose.connect(config.mongoConn);
-
+    var db = mongoose.connect(config.mongoConn, {
+    useMongoClient: true
+    });
     require("../app/models/model.order.server");
     require("../app/models/model.ordersCache.server");
     return db;
